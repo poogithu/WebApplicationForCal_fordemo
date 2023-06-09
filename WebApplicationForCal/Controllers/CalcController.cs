d11a7ce43460f9926a34ff23b663262789da3017
Pooja vyavhare <pooja.vyavhare@bps.uk.atos.net>
2023-06-09 15:51:45 +0530

    Add project files.

---
+++ WebApplicationForCal/Controllers/CalcController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationForCal.Models;
+
+namespace WebApplicationForCal.Controllers
+{
+    public class CalcController : Controller
+    {
+        // GET: Calc
+        public ActionResult Index()
+        {
+            return View(new Calc());
+        }
+        [HttpPost]
+        public ActionResult Index(Calc c,string calculate)
+        {
+            if (calculate == "add") 
+            {
+                c.tot = c.no1 + c.no2;
+
+            }
+            else if (calculate == "min") 
+            {
+                c.tot = c.no1 - c.no2;
+
+
+            }
+            else if (calculate == "sub") 
+            {
+                c.tot = c.no1 * c.no2;
+
+
+            }
+            else
+            {
+                c.tot = c.no1 - c.no2;
+            }
+            return View(c);
+        }
+    }
+}
---
+++ WebApplicationForCal/Models/Calc.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationForCal.Models
+{
+    public class Calc
+    {
+        public int no1 { get; set; }
+        public int no2 { get; set; }
+        public int tot { get; set; }
+    }
+}
